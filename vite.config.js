2e38ae0856d4f10eaeedbd190ca424c7368d0929
Rohan Godha <rohan.godha@navikenz.com>
2025-08-14 15:21:38 +0530

    Setup GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/homerun-cart/",
+  base: "/homerun-cart",
   plugins: [react()]
 });
